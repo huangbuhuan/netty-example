93e081823c16271b8e05d5eb9fcf77ed3f0695d1
黄步欢 <i@buhuan.me>
2020-04-30 01:20:49 +0800

    add http server

---
+++ http/src/main/java/me/buhuan/HttpServer.java
@@ -0,0 +1,63 @@
+package me.buhuan;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.proxy.HttpProxyHandler;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
+
+import javax.net.ssl.SSLException;
+import java.net.InetSocketAddress;
+import java.security.cert.CertificateException;
+
+/**
+ * @author hbh
+ * @date 2020/4/29
+ */
+public class HttpServer {
+
+	private final int port;
+
+	public HttpServer(int port) {
+		this.port = port;
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		new HttpServer(8000).start();
+	}
+
+	public void start() throws InterruptedException {
+		EventLoopGroup boss = new NioEventLoopGroup();
+		EventLoopGroup work = new NioEventLoopGroup();
+		try {
+			ServerBootstrap b = new ServerBootstrap();
+			b.group(boss, work)
+				.channel(NioServerSocketChannel.class)
+				.localAddress(new InetSocketAddress(port))
+				.childHandler(new ChannelInitializer<Channel>() {
+					@Override
+					protected void initChannel(Channel ch)  {
+						ChannelPipeline pipeline = ch.pipeline();
+						// 添加http解码器
+						pipeline.addLast("codec", new HttpServerCodec());
+						// 添加消息聚合器
+						pipeline.addLast(new HttpObjectAggregator(64 * 1024));
+						// 添加自定义处理器
+						pipeline.addLast(new HttpsServerHandler());
+					}
+				});
+			ChannelFuture f = b.bind().sync();
+			f.channel().closeFuture().sync();
+		} finally {
+			boss.shutdownGracefully().sync();
+			work.shutdownGracefully().sync();
+		}
+	}
+}
---
+++ http/src/main/java/me/buhuan/HttpsServerHandler.java
@@ -0,0 +1,38 @@
+package me.buhuan;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.*;
+import io.netty.util.AsciiString;
+
+/**
+ * @author hbh
+ * @date 2020/4/29
+ */
+public class HttpsServerHandler extends SimpleChannelInboundHandler<HttpObject> {
+
+	@Override
+	protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
+		if (msg instanceof HttpRequest) {
+			String content = "hello world";
+			FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, Unpooled
+				.wrappedBuffer(content.getBytes()));
+			response.headers().set("Content-Type", "text/plain; charset=UTF-8");
+			response.headers().set("Content-Length", response.content().readableBytes());
+			ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+		}
+	}
+
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+		ctx.flush();
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		cause.printStackTrace();
+		ctx.close();
+	}
+}
